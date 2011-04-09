ff66bcd707efa8b36c7120f6c4c1e0b8e916e924
curiousleo <curiousleo@ymail.com>
2011-04-09 21:02:03 +0200

    Added _EXT, _PATH options

--- textile/html_gen.py
+++ textile/html_gen.py
@@ -8,35 +8,41 @@ from codecs import open as copen
 from os import path as os_path, curdir
 
 TEXTILE_PATH = HTML_PATH = curdir
+TEXTILE_EXT, HTML_EXT = 'textile', 'html'
+TEMPLATE_PATH = 'template.' + HTML_EXT
+NAVI = []
 
-def html_gen(page, title=None, navi=[], template_path='template.htm'):
+def html_gen(page, title=None):
    if not title: title = page.title()
-   input_path = os_path.join(TEXTILE_PATH, page.lower() + '.textile')
-   output_path = os_path.join(HTML_PATH, page.lower() + '.htm')
+   input_path = os_path.join(TEXTILE_PATH, page.lower() + TEXTILE_EXT)
+   output_path = os_path.join(HTML_PATH, page.lower() + HTML_EXT)
 
    text = textile(open(input_path).read())
-   template = Template(open(template_path).read())
+   template = Template(open(TEMPLATE_PATH).read())
    context = {'title': title, 'content': text.decode('utf-8'), 
-      'navigation': navi}
+      'navigation': NAVI}
    html = template.render(context)
 
    with copen(output_path, encoding='latin-1', mode='w') as output:
       output.write(html)
-      print output_path, "sucessfully written."
+      print output_path, 'done.'
 
-if __name__ == "__main__":
-   if   len(argv) == 1: pass
-   elif len(argv) == 2: TEXTILE_PATH = HTML_PATH = argv[1]
-   elif len(argv) == 3: TEXTILE_PATH, HTML_PATH = argv[1], argv[2]
-   else: exit(-1)
-
+if __name__ == '__main__':
    config = ConfigParser()
    config.read('site.cfg')
 
-   template_path = config.get('template', 'path')
-   navi = [{'url': page + '.htm', 'title': title} \
-      for page, title in config.items('menu')]
+   if len(argv) == 1:
+      TEXTILE_PATH = config.get('input', 'dir')
+      HTML_PATH = config.get('output', 'dir')
+   elif len(argv) == 3: TEXTILE_PATH, HTML_PATH = argv[1], argv[2]
+   else: exit(-1)
 
-   for page, title in config.items('menu'):
-      try: html_gen(page, title, navi, template_path)
+   TEXTILE_EXT = config.get('input', 'ext')
+   HTML_EXT = config.get('output', 'ext')
+   TEMPLATE_PATH = config.get('template', 'path')
+   NAVI = [{'url': page + HTML_EXT, 'title': title} \
+      for page, title in config.items('navi')]
+
+   for page, title in config.items('navi'):
+      try: html_gen(page, title)
       except IOError, e: print e
