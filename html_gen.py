10ca4f15291bbff28a036c264a237f36aab7aa54
curiousleo <curiousleo@ymail.com>
2011-04-09 16:19:10 +0200

    Using codecs package; included navigation read from [menu] in site.cfg

--- html_gen.py
+++ html_gen.py
@@ -2,23 +2,27 @@ from jinja2 import Template
 from textile import textile
 from sys import argv, exit
 from ConfigParser import ConfigParser
+import codecs
 
-def html_gen(page, title=None, template_path='template.htm'):
+def html_gen(page, title=None, navi=[], template_path='template.htm'):
    if not title: title = page.title()
    input_path = page.lower() + '.textile'
    output_path = page.lower() + '.htm'
 
-   text = textile("".join(l for l in open(c['input_path'])))
-   template = Template("".join(l for l in open(c['template_path'])))
-   context = {'title': c['title'], 'content': text.decode('utf-8')}
-   html = template.render(context).encode('utf-8')
-   with open(c['output_path'], 'w') as output:
+   text = textile("".join(l for l in open(input_path)))
+   template = Template("".join(l for l in open(template_path)))
+   context = {'title': title, 'content': text.decode('utf-8'), 'navigation': navi}
+   html = template.render(context)
+   with codecs.open(output_path, encoding='latin-1', mode='w') as output:
       output.write(html)
-      print c['output_path'], "sucessfully written. Bye!"
+      print output_path, "sucessfully written."
 
 if __name__ == "__main__":
    config = ConfigParser()
    config.read('site.cfg')
    template_path = config.get('template', 'path')
+   navi = [{'url': page + '.textile', 'title': title} \
+      for page, title in config.items('menu')]
    for page, title in config.items('menu'):
-      html_gen(page, title, template_path)
+      try: html_gen(page, title, navi, template_path)
+      except IOError, e: print e
