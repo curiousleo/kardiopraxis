600f08f0fa256400abf70520e5239fbdca13ffbb
curiousleo <curiousleo@ymail.com>
2011-04-09 16:23:05 +0200

    Removed ugly l for l in open(...) hack

--- html_gen.py
+++ html_gen.py
@@ -9,10 +9,11 @@ def html_gen(page, title=None, navi=[], template_path='template.htm'):
    input_path = page.lower() + '.textile'
    output_path = page.lower() + '.htm'
 
-   text = textile("".join(l for l in open(input_path)))
-   template = Template("".join(l for l in open(template_path)))
+   text = textile(open(input_path).read())
+   template = Template(open(template_path).read())
    context = {'title': title, 'content': text.decode('utf-8'), 'navigation': navi}
    html = template.render(context)
+
    with codecs.open(output_path, encoding='latin-1', mode='w') as output:
       output.write(html)
       print output_path, "sucessfully written."
@@ -20,9 +21,11 @@ def html_gen(page, title=None, navi=[], template_path='template.htm'):
 if __name__ == "__main__":
    config = ConfigParser()
    config.read('site.cfg')
+
    template_path = config.get('template', 'path')
    navi = [{'url': page + '.textile', 'title': title} \
       for page, title in config.items('menu')]
+
    for page, title in config.items('menu'):
       try: html_gen(page, title, navi, template_path)
       except IOError, e: print e
