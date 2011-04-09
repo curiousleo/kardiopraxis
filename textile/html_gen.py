9b7bf2dc3587c89532d48e44232ab4b823dce247
curiousleo <curiousleo@ymail.com>
2011-04-09 20:40:09 +0200

    Added support for TEXTILE_PATH and HTML_PATH.

--- textile/html_gen.py
+++ textile/html_gen.py
@@ -1,17 +1,23 @@
+# vim: expantab tabstop=3 coding=utf8
+
 from jinja2 import Template
 from textile import textile
 from sys import argv, exit
 from ConfigParser import ConfigParser
 from codecs import open as copen
+from os import path as os_path, curdir
+
+TEXTILE_PATH = HTML_PATH = curdir
 
 def html_gen(page, title=None, navi=[], template_path='template.htm'):
    if not title: title = page.title()
-   input_path = page.lower() + '.textile'
-   output_path = page.lower() + '.htm'
+   input_path = os_path.join(TEXTILE_PATH, page.lower() + '.textile')
+   output_path = os_path.join(HTML_PATH, page.lower() + '.htm')
 
    text = textile(open(input_path).read())
    template = Template(open(template_path).read())
-   context = {'title': title, 'content': text.decode('utf-8'), 'navigation': navi}
+   context = {'title': title, 'content': text.decode('utf-8'), 
+      'navigation': navi}
    html = template.render(context)
 
    with copen(output_path, encoding='latin-1', mode='w') as output:
@@ -19,6 +25,11 @@ def html_gen(page, title=None, navi=[], template_path='template.htm'):
       print output_path, "sucessfully written."
 
 if __name__ == "__main__":
+   if   len(argv) == 1: pass
+   elif len(argv) == 2: TEXTILE_PATH = HTML_PATH = argv[1]
+   elif len(argv) == 3: TEXTILE_PATH, HTML_PATH = argv[1], argv[2]
+   else: exit(-1)
+
    config = ConfigParser()
    config.read('site.cfg')
 
